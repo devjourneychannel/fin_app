1a048ab55861f7262a0eec7e08b3bbbbaa2039da
NilsonCarlosSaraf <142173393+NilsonCarlosSaraf@users.noreply.github.com>
2024-03-10 12:11:53 -0300

    feat: add new folder structures for components and styles (#3)
    * update
    
    * feat: add new folder structures for components and styles

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>HOME PAGE</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
